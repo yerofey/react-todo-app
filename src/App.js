55e78c24f7f3b45762433689ef6ebce80cd5306e
Yerofey S <pm@yerofey.dev>
2020-04-09 22:09:00 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,166 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react';
+// shortid
+import shortid from 'shortid';
+// checkbox
+import Checkbox from 'rc-checkbox';
+import 'rc-checkbox/assets/index.css';
+// icons
+import { IconContext } from 'react-icons';
+import { FaTrash } from 'react-icons/fa';
 
-function App() {
+const App = () => {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useLocalStorage('list', []);
+
+  const onNewTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value);
+  }, []);
+
+  const onNewTodoSubmit = useCallback((event) => {
+    event.preventDefault();
+
+    if (!newTodo.trim()) {
+      return;
+    }
+
+    // add new item
+    setTodos([
+      ...todos,
+      {
+        id: shortid.generate(),
+        content: newTodo,
+        done: false,
+        timestamp: new Date()
+      }
+    ]);
+
+    // reset input
+    setNewTodo('');
+  }, [newTodo, todos, setTodos]);
+
+  const changeTodo = useCallback((todo, index) => (event) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1, {
+      ...todo,
+      done: !todo.done
+    });
+    setTodos(newTodos);
+  }, [todos, setTodos]);
+
+  const deleteTodo = useCallback((todo) => (event) => {
+    setTodos(todos.filter(item => item !== todo));
+  }, [todos, setTodos]);
+
+  const markAllDone = useCallback(() => {
+    const newTodos = todos.map(item => {
+      return {
+        ...item,
+        done: true
+      };
+    });
+    setTodos(newTodos);
+  }, [todos, setTodos]);
+
+  const deleteAll = useCallback(() => {
+    setTodos([]);
+  }, [setTodos]);
+
+  useEffect(() => {
+    if (todos.length === 0) {
+      document.getElementById('buttons').style.display = 'none';
+      document.getElementById('message').style.display = 'block';
+    } else {
+      document.getElementById('buttons').style.display = 'block';
+      document.getElementById('message').style.display = 'none';
+    }
+
+    window.localStorage.setItem('list', JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <div className="item-list">
+          <div className="buttons" id="buttons">
+            <button className="done-button" id="checkAllButton" onClick={markAllDone}>
+              <Checkbox
+                checked
+                onChange={() => {}}
+                disabled={false}
+              />
+              <span>
+                Check All
+              </span>
+            </button>
+            <button className="delete-button" id="deleteAllButton" onClick={deleteAll}>
+              <IconContext.Provider value={{ color: '#9a9a9a', size: '14px' }}>
+                <FaTrash/>
+              </IconContext.Provider>
+              <span>
+                Delete All
+              </span>
+            </button>
+          </div>
+          <div className="message" id="message">
+            You don't have any tasks yet :(
+          </div>
+          
+          {todos.length !== 0 && todos.map((todo, index) => (
+            <label className={'item' + (todo.done ? ' -done' : '')} htmlFor={'check-' + todo.id} key={todo.id}>
+              <Checkbox
+                id={'check-' + todo.id} 
+                checked={todo.done}
+                onChange={changeTodo(todo, index)}
+              />
+              <span className="content">
+                {todo.content}
+              </span>
+              <button className="delete-button" onClick={deleteTodo(todo)}>
+                <FaTrash/>
+              </button>
+            </label>
+          ))}
+        </div>
+        <form className="todo-form" onSubmit={onNewTodoSubmit}>
+          <div className="line">
+            <input 
+              id="newTodo"
+              name="new-todo"
+              value={newTodo} 
+              onChange={onNewTodoChange}
+              autoFocus
+              autoComplete="off"
+              placeholder="Add new task"
+            />
+          </div>
+        </form>
+      </main>
     </div>
   );
+};
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
 export default App;
